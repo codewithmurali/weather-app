26cdda5bd95ae27125c68e522d150ddf9c41c2ee
codewithmurali <muralikumar.admire@gmail.com>
2024-01-05 17:29:20 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Weatherapp from "./Components/Weather-App/weatherapp";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Weatherapp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Weather-App/weatherapp.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "./weatherapp.css";
+
+import clear_icon from "../../assets/clear.png";
+import cloud_icon from "../../assets/cloud.png";
+import drizzle_icon from "../../assets/drizzle.png";
+import humidity_icon from "../../assets/humidity.png";
+import rain_icon from "../../assets/rain.png";
+import search_icon from "../../assets/search.png";
+import snow_icon from "../../assets/snow.png";
+import wind_icon from "../../assets/wind.png";
+
+const Weatherapp = () => {
+  let api_key = "5c42912eb09eee021741318771ad32a1";
+
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temperature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+
+    humidity[0].innerHTML = data.main.humidity + "%";
+    wind[0].innerHTML = data.wind.speed + " km/h";
+    temperature[0].innerHTML = data.main.temp + "C";
+    location[0].innerHTML = data.name;
+  };
+
+  return (
+    <div className="container">
+      <div className="top-bar">
+        <input
+          type="text"
+          className="cityInput"
+          placeholder="Enter the City name"
+        ></input>
+        <div className="search-icon" onClick={() => search()}>
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-temp">24</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind_icon} alt="" icon />
+          <div className="data">
+            <div className="wind-rate">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weatherapp;
